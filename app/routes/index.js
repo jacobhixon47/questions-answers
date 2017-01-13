6db73384f3aeb33196da21cb8854cbee0d427148
Jacob Hixon <jacobhixon47@gmail.com>
2017-01-13 13:48:44 -0800

    add answer functionality to add answers to questions and display them

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question')
-  },
-  
+  },  
   actions: {
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
